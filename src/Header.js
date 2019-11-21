54996cb3d532e3bf2c07f711b1e7900db5beeb3a
SamuraiViking <nelson67@stolaf.edu>
2019-11-21 15:39:05 -0600

    make warning pretty

--- src/Header.js
+++ src/Header.js
@@ -5,7 +5,19 @@ import Alert from 'react-bootstrap/Alert'
 const Header = (props) => {
     const Warning = () => {
         if(props.diets.includes(1) && props.diets.includes(4)) {
-            return <Alert className="warning" variant='primary'>No V will appear because VG is selected</Alert>
+            return (
+                <Alert className="warning" variant='primary'>
+                    You won't see any
+                    <div className="icon center mb-10">
+                        <img src="https://legacy.cafebonappetit.com/assets/cor_icons/menu-item-type-c9d18b.png?v=1528869517"alt="vegitarian"/>
+                    </div> 
+                    because
+                    <div className="icon center mb-10">
+                            <img src="https://legacy.cafebonappetit.com/assets/cor_icons/menu-item-type-668e3c.png?v=1528869577" alt="vegan"/>
+                    </div>
+                    is selected
+                </Alert>
+            )    
         }
         return null
     }
