68489d4393f5356283aec9c6cf3073a229f4bcd7
SamuraiViking <nelson67@stolaf.edu>
2019-11-20 21:38:59 -0600

    show less/show more functionality

--- src/Items.js
+++ src/Items.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as CafData from './cafData.json'
 import Icons from './Icons'
 import Button from 'react-bootstrap/Button';
@@ -24,31 +24,48 @@ const Item = (props) => {
 }
 
 const unfilteredItem = (item, filters) => {
-    if (filters.tier === Number(item.tier)) return true
-    else if (filters.icon in item.cor_icon) return true
-    return false
+    return true
 }
 
 const Items = (props) => {
+    const [showMoreItems, setShowMoreItems] = useState(false)
+    const [btnText, setBtnText] = useState("show more")
     let items = []
+    let moreItems = []
     props.itemIDs.forEach((itemID, i) => {
         let item = CafData[0].items[itemID];
         let filters = {
             tier: 1,
-            icon: "4"
         }
         if(unfilteredItem(item, filters)) {
-            items.push(
+            let itemElem = (
                 <Item
-                    key={i}
-                    label={item.label}
-                    icons={item.cor_icon}
-                    description={item.description}
+                key={i}
+                tier={item.tier}
+                label={item.label}
+                icons={item.cor_icon}
+                description={item.description}
                 />
             )
+            Number(item.tier) === 1 ?
+                items.push(itemElem)
+                :
+                moreItems.push(itemElem)
         }
     })
-    return <div className="items">{items}</div>
+    const handleClick = () => {
+        setShowMoreItems(!showMoreItems)
+        let newBtnText = ""
+        newBtnText = btnText === "show more" ? "show less" : "show more"
+        setBtnText(newBtnText)
+    }
+    return (
+        <div className="items">
+            {items}
+            { showMoreItems ? moreItems : null }
+            { moreItems.length > 0 ? <div onClick={handleClick} >{btnText}</div> : null }
+        </div>
+    )
 }
 
 export default Items
